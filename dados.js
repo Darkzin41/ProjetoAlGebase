ca763139283ce7396f6c2857993073aa670ebe14
Darkzin41 <arielasafe09@gmail.com>
2024-09-06 17:24:21 -0300

    todos os dados atuais atualizados

---
+++ dados.js
@@ -0,0 +1,45 @@
+let dados = [
+  {
+    titulo: "História e Tradição",
+    descricao:
+      "Uma jornada desde os primórdios do esporte no Brasil até a formação de ídolos e a construção de uma identidade única para o basquete brasileiro.",
+    link: "https://www.todamateria.com.br/historia-do-basquete/",
+    tags: "historia tradição basquete brasileiro origem ídolos",
+  },
+  {
+    titulo: "Competições e Ligas",
+    descricao:
+      "A organização e o desenvolvimento do basquete através de competições nacionais e internacionais, com destaque para a NBB e a seleção brasileira.",
+    link: "https://pt.wikipedia.org/wiki/Categoria:Competições_de_basquetebol_do_Brasil",
+    tags: "competições ligas NBB seleção brasileira campeonatos",
+  },
+  {
+    titulo: "Brasil e suas conquistas",
+    descricao:
+      "O Brasil é mundialmente reconhecido por sua paixão pelo esporte e por suas inúmeras conquistas. Em diversas modalidades, nossos atletas deixaram sua marca e levaram o nome do país para o topo do pódio.",
+    link: "https://www.cbb.com.br/conquistas",
+    tags: "Brasil basquete brasileiro mundo internacional ligas",
+  },
+  {
+    titulo: "Cultura e Sociedade",
+    descricao:
+      "A presença do basquete na cultura popular, a relação do esporte com a sociedade e a importância da modalidade para a juventude.",
+    link: "https://medium.com/@laboratoriodejornalismo2022/sobe-a-laranja-o-crescimento-do-basquete-e-sua-cultura-no-brasil-cd2767c83617",
+    tags: "cultura sociedade basquete importância social humano",
+  },
+  {
+    titulo: "Tecnologia e Inovação",
+    descricao:
+      "Os avanços tecnológicos e as inovações no basquete, desde a criação de equipamentos até a análise de desempenho e a preparação física dos atletas.",
+    link: "https://leianoticias.com.br/basquete-e-tecnologia-como-as-inovacoes-moldam-o-futuro-do-esporte-no-brasil/",
+    tags: "avanço futuro tecnologia digital desenvolvimento tendências",
+  },
+
+  {
+    titulo: "Curiosidades e Dados",
+    descricao:
+      "Informações curiosas e estatísticas sobre o basquete, seus recordes, seus personagens e sua evolução ao longo dos anos.",
+    link: "https://www.suapesquisa.com/educacaoesportes/curiosidades_basquete.htm",
+    tags: "fatos estatísticas números história",
+  },
+];
